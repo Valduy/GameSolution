4fac4d01279694dac825821dd2a6d7797d8d8b45
Valduy <whnpir@gmail.com>
2021-06-15 20:53:03 +0300

    Перенес фейкового клиента на .NET

--- FakeClient/Program.cs
+++ FakeClient/Program.cs
@@ -1,17 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Runtime.CompilerServices;
 using System.Text;
-using System.Text.Json;
-using System.Threading;
 using System.Threading.Tasks;
 using Connectors.HolePuncher;
 using Connectors.MatchConnectors;
-using Network;
-using Network.Messages;
 
 namespace FakeClient
 {
